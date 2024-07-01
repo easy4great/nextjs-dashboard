775a2948244e5536bbddca5d1b06e33318639084
Oladeji Ezekiel <easy4great@gmail.com>
2024-07-01 19:59:08 +0100

    version 1

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>customers Page in the leave segment level</p>;
+  }
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>customer invoice Page in the leave segment level</p>;
+  }
